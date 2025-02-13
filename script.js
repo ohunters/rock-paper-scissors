84c7412e26e5bfba8402a284e70e701fc47047c5
Tom Hunt <tom@visualr.co.uk>
2025-02-13 15:20:45 +0000

    Create logic for computers choice

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hello World")
+// Game Variables
+let computerChoice = "";
+let humanChoice = "";
+let computerScore = 0;
+let humanScore = 0;
+let roundNumber = 0;
+let gameWinner = ""
+let randomNumber = Math.floor(Math.random() * 3 + 1)
+
+
+// Logic for computers choice
+if (randomNumber === 1) {
+    computerChoice = "rock";
+} else if (randomNumber === 2) {
+    computerChoice = "paper"
+} else {
+    computerChoice = "scissors"
+};
+
+console.log(computerChoice)
+
+// Get human input and assign it human choice variable and make it lower case
+
+// Create play round function which takes computer choice and human choice
+// If computer pick is the same as human return draw
+
+// If computer picks rock and human picks scissors return computer wins
+// If computer picks paper and human picks rock return computer wins
+// If computer picks scissors and human picks paper return computer wins
+
+// If any other outcome return computer wins
+
+// Add one to round variable
+
+
+
