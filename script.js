2a8665862d97eb289b037291c0bc64f45920a2eb
Tom Hunt <tom@visualr.co.uk>
2025-02-13 15:49:32 +0000

    Make computer choice and human choice functions

--- script.js
+++ script.js
@@ -9,20 +9,27 @@ let randomNumber = Math.floor(Math.random() * 3 + 1)
 
 
 // Logic for computers choice
-if (randomNumber === 1) {
-    computerChoice = "rock";
-} else if (randomNumber === 2) {
-    computerChoice = "paper"
-} else {
-    computerChoice = "scissors"
-};
+function getComputerChoice {
+    if (randomNumber === 1) {
+        computerChoice = "rock";
+    } else if (randomNumber === 2) {
+        computerChoice = "paper"
+    } else {
+        computerChoice = "scissors"
+    };
+}
+
 
 
 // Logic for humans choice
-humanChoice = prompt("Please pick either Rock, Paper or Scissors")
-humanChoice = humanChoice.toLowerCase();
+function getHumanChoice {
+    humanChoice = prompt("Please pick either Rock, Paper or Scissors")
+    humanChoice = humanChoice.toLowerCase();
+};
+
 
 // Create play round function which takes computer choice and human choice
+
 // If computer pick is the same as human return draw
 
 // If computer picks rock and human picks scissors return computer wins
