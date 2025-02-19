6b0dbcf14adc665143fd810d5f72b322bd17d008
Tom Hunt <tom@visualr.co.uk>
2025-02-19 14:00:37 +0000

    Create game winner and reset button after either player wins 5 rounds

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let selectChoiceHeader = document.querySelector(".select-choice-header")
 let optionButtonRock = document.querySelector(".option-button-rock");
 let optionButtonPaper = document.querySelector(".option-button-paper");
 let optionButtonScissors = document.querySelector(".option-button-scissors");
+let resultContainer = document.querySelector(".result-container");
 let resultContainerParagraph = document.querySelector(".result-container-paragraph");
+let resetButton;
 
 // Game Variables
 let computerScore = 0;
@@ -17,7 +19,7 @@ function getComputerChoice() {
 
 // Logic for a single round
 function playRound(computerChoice, humanChoice) {
-    if (computerChoice === humanChoice) {
+    if (computerChoice === humanChoice ) {
         resultContainerParagraph.textContent = `Draw! You both picked ${computerChoice}. Score - You: ${humanScore}, Computer: ${computerScore}`;
     } else if (
         (computerChoice === "Rock" && humanChoice === "Scissors") ||
@@ -29,8 +31,43 @@ function playRound(computerChoice, humanChoice) {
     } else {
         humanScore++;
         resultContainerParagraph.textContent = `You win! ${humanChoice} beats ${computerChoice}. Score - You: ${humanScore}, Computer: ${computerScore}`;
+    };
+
+    if (computerScore === 5 || humanScore === 5) {
+        if (computerScore === 5) {
+            resultContainerParagraph.textContent = `You lose the game! ${computerChoice} beats ${humanChoice}. Final Score - You: ${humanScore}, Computer: ${computerScore}. Please press the restart button to play again!`;
+        } else {
+            resultContainerParagraph.textContent = `You win the game! ${humanChoice} beats ${computerChoice}. Final Score - You: ${humanScore}, Computer: ${computerScore}. Please press the restart button to play again!`;
+        }
+
+        optionButtonRock.disabled = true;
+        optionButtonPaper.disabled = true;
+        optionButtonScissors.disabled = true;
+
+        if (!resetButton) {
+            resetButton = document.createElement("button");
+            resetButton.textContent = "RESET GAME";
+            resultContainer.appendChild(resetButton);
+            resetButton.addEventListener("click", restartGame);
+        }
+    }
+};
+
+function restartGame() {
+    computerScore = 0;
+    humanScore = 0;
+    optionButtonRock.disabled = false;
+    optionButtonPaper.disabled = false;
+    optionButtonScissors.disabled = false;
+    resultContainerParagraph.textContent = "";
+
+    if (resetButton) {
+        resultContainer.removeChild(resetButton);
+        resetButton = null;  
     }
 }
+
+
 
 optionButtonRock.addEventListener("click", () => {
     let computerChoice = getComputerChoice();
@@ -46,21 +83,4 @@ optionButtonScissors.addEventListener("click", () => {
 });
 
 
-// Main game loop
-/* 
-while (computerScore < 5 && humanScore < 5) {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-
-    playRound(computerChoice, humanChoice);
-
-    if (computerScore === 5) {
-        alert("You have lost the game!");
-        break;
-    } else if (humanScore === 5) {
-        alert("You have won the game!");
-        break;
-    }
-}
-*/
 
